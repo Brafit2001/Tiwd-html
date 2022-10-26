2830723f145c09a8a93ae0cfa83ac977643d60b4
PabloRius <100428976@alumnos.uc3m.es>
2022-10-26 10:51:04 +0200

    Add tickets page + admin buttons on events cards

--- script/main.js
+++ script/main.js
@@ -1,10 +1,17 @@
 
-
-/*Carga de header y footer a todas las pag*/
+/*Loads header and footer*/
 $(function(){
     $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
+
+$(document).ready(setup);
+let admin = false;
+
+function load_header_footer(){
+    $("#header").innerHTML = "footer.html";
+    $("#footer").load("footer.html");
+}
 
 
 /*Menu pop up*/
@@ -15,5 +22,33 @@ function popup(){
     }else {
         container.style.display = 'flex'
     }
+}
+
+function enter_admin_mode(){
+    admin = true;
+    console.log("Set admin true");
+}
+
+function checkAdmin(){
+    console.log("Checking...")
+    if (admin === true){
+        $(".admin-service").css("display", "flex");
+        console.log("Changed css");
+    }
+}
+
+function addTicketsPage(){
+    window.location.href = "add-tickets.html";
+}
+
+function eventsPage(){
+    window.location.href = "experience.html";
+}
+
+function setup(){
+    load_header_footer();
+    $(".ticket-col").click(addTicketsPage);
+    $(".card-img").click(eventsPage);
+    setInterval(checkAdmin, 3000);
 }
 
